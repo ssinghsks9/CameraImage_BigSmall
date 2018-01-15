06e5e598af4f6bb622f237caf1fa11ef8b4de421
sandeep <sandeep.singh@incedoinc.com>
2018-01-16 00:07:12 +0530

    first commit

---
+++ app/src/main/java/com/example/android/photobyintent/provider/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package com.example.android.photobyintent.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by sandeep on 15/01/18.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
